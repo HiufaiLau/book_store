a7393425b491401cec81cdd696ce249743a708fa
hobbit lau <hiufailau.41@gmail.com>
2018-10-30 12:03:36 +0100

    change

--- books.js
+++ books.js
@@ -1,42 +1,67 @@
 //showing the book images
-function showBooks(data) {
- data.forEach(function (book) {
-  var img = new Image();
-  img.src = book.portada;
-  img.setAttribute("class", "books-img");
-  img.setAttribute("alt", "books");
-  document.getElementById("img-container").appendChild(img);
-  console.log(img);
- });
-}
-
-
+//function showBooks(data) {
+// data.forEach(function (book) {
+//  var img = new Image();
+//  img.src = book.portada;
+//  img.setAttribute("class", "books-img");
+//  img.setAttribute("alt", "books");
+//  document.getElementById("img-container").appendChild(img);
+//  console.log(img);
+// });
+//}
+//
+//
 //book title and description  ,,  more info button
-function bookInfo(text) {
- var bookTitleDescription = document.getElementsByClassName("bookInformation")[0];
- console.log(bookTitleDescription);
- var items = text;
- for (var i = 0; i < items.length; i++) {
-  
-  //book title
-  var h = document.createElement("h2");
-  h.innerHTML = items[i].titulo;
-  bookTitleDescription.appendChild(h);
-  
-  //book description
-  var p = document.createElement("p");
-  p.innerHTML = items[i].descripcion;
-  bookTitleDescription.appendChild(p);
-  
-  //link for more info 
-  var a = document.createElement("a");
-  a.href = items[i].detalle;
- a.getAttribute("href");
-  a.innerText="More Info"
- bookTitleDescription.appendChild(a);
-  console.log(items);
- }
-}
+//function bookInfo(text) {
+// var bookTitleDescription = document.getElementsByClassName("bookInformation")[0];
+// console.log(bookTitleDescription);
+// var items = text;
+// for (var i = 0; i < items.length; i++) {
+//
+////  book title
+//  var h = document.createElement("h2");
+//  h.innerHTML = items[i].titulo;
+//  bookTitleDescription.appendChild(h);
+//
+//  //book description
+//  var p = document.createElement("p");
+//  p.innerHTML = items[i].descripcion;
+//  bookTitleDescription.appendChild(p);
+//
+//  link for more info 
+//  var a = document.createElement("a");
+//  a.href = items[i].detalle;
+//  a.getAttribute("href");
+//  a.innerText = "More Info"
+//  bookTitleDescription.appendChild(a);
+//  console.log(items);
+// }
+// showTheDetails(showOnebookDetail);
+//}
+//////
+//////function showTheDetails(bookDetails) {
+//////
+////// //to filter the data
+////// //var showOneBookDetail=[];
+////// var showOneBookDetail = myBook.filter(oneBook => bookDetails.includes(oneBook.portada))
+//////console.log(showOneBookDetail);
+//////}
+//
+//
+////searching book 
+//function myFunction() {
+// var search = document.getElementById("myInput");
+// for (var i = 0; i < items.length; i++) {
+//  document.getElementsByClassName("bookInformation")[0];
+//  console.log(search);
+// }
+//}
+//function changeEventHandler() {
+// 
+// var newTable = myMembers.filter(oneMember => box.includes(oneMember.party) && opt == oneMember.state)
+// return  bookInfo(data);
+// 
+//}
 //function moreInfo(info) {
 // info.forEach(function (book) {
 //  console.log(info);
@@ -49,3 +74,30 @@ function bookInfo(text) {
 //  console.log(a);
 // });
 //}
+
+function printBooks(myBooks){
+ var template = "";
+ myBooks.forEach(oneBook => {
+   template += //template +
+    `
+    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+		<div class="flipper">
+			<div class="front">
+				<div>
+                  <img src="${ oneBook.portada}" height="400" width="250"> 
+                </div>
+			</div>
+			<div class="back">
+				<div>
+                   <h1>${oneBook.titulo}</h1>
+                   <p>${oneBook.descripcion}</p>
+                   <a href="${oneBook.detalle}">More Info</a>
+                </div>
+			</div>
+		</div>
+	</div> 
+    `
+ })
+ 
+ document.getElementById('books').innerHTML = template;
+}
